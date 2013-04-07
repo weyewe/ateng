8d87741651c12b117e5947f6bff25507133193f0
weyewe <w.yunnal@gmail.com>
2013-04-07 13:47:46 +0700

    initial setup with extjs

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,73 @@
+class Api::SessionsController < Api::BaseApiController
+  before_filter :say_moron
+  before_filter :authenticate_user!, :except => [:create, :destroy, :say_hi ]
+  before_filter :ensure_params_exist, :except => [:say_hi, :destroy, :authenticate_auth_token]
+  # skip_before_filter :authenticate_user! #, :only => [:create, :destroy, :say_hi ]
+  respond_to :json
+
+  def create
+    
+    puts "**********************\n"*10
+    puts "In the create"
+    resource = User.find_for_database_authentication(:email => params[:user_login][:email])
+    puts "The resource: #{resource.inspect}"
+    return invalid_login_attempt unless resource
+    
+    puts "Resurce is valid"
+ 
+    if resource.valid_password?(params[:user_login][:password])
+      puts "The password is valid"
+      sign_in(:user, resource)
+      resource.ensure_authentication_token!
+      render :json=> {:success=>true, 
+                      :auth_token=>resource.authentication_token, 
+                      :email=>resource.email,
+                      :role => resource.role.to_json
+              }
+      return
+    end
+    
+    puts "The password is invalid"
+    invalid_login_attempt
+  end
+  
+  def say_hi
+    render :json=> {:success=>true, 
+                    :msg => "Server: This is your coffee!"
+            }
+  end
+  
+  def authenticate_auth_token
+    render :json => {:success => true }
+  end
+ 
+  def destroy
+    # resource = User.find_for_database_authentication(:email => params[:user_login][:email])
+    resource = User.find_by_authentication_token( params[:auth_token])
+    if resource
+      resource.authentication_token = nil
+    end
+    # resource.save
+    
+    if resource and resource.save
+      render :json=> {:success=>true}
+    else
+      render :json=> {:success=>false}
+    end
+    
+  end
+ 
+  protected
+  def ensure_params_exist
+    puts "We are inside the ensure params_exist"
+    return unless params[:user_login].blank?
+    puts "haiz, params does not exists!!!!!"
+    render :json=>{:success=>false, :message=>"missing user_login parameter"}, :status=>422
+  end
+ 
+  def invalid_login_attempt
+    puts "888\n"*5
+    puts "Invalid login attempt"
+    render :json=> {:success=>false, :message=>"Error with your login or password"}, :status=>401
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ateng::Application.config.secret_token = 'fcc3646ef3e69375dbb2de22d1f6b769e363c234f6f33122c3367035b3b745c5b8d156d4515c3a13736b34fd020c464f55f87aa87a1581eacf4717b8314dd53d'
