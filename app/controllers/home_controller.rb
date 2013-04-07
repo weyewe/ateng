dc0f2f48a97f1010cf1ceb4d7ce3d6d01f11aadb
weyewe <w.yunnal@gmail.com>
2013-04-07 13:54:17 +0700

    base routes

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ApplicationController
+  
+  def extjs
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Ateng::Application.routes.draw do
   devise_for :users
-  
+  root :to => 'home#extjs'
   namespace :api do
     devise_for :users
     match 'authenticate_auth_token' => 'sessions#authenticate_auth_token', :as => :authenticate_auth_token
